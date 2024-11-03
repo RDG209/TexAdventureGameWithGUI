794ba23448dfcfbfcfecf3def706f481c021d153
RDG209 <rgaeta209@gmail.com>
2024-11-02 20:10:42 -0700

    Add files via upload

---
+++ TextAdventureGameGUI/src/Monsters/Monster_Kobold.java
@@ -0,0 +1,13 @@
+package Monsters;
+
+public class Monster_Kobold extends SuperMonster {
+	
+	public Monster_Kobold() {
+		
+		name = "Kobold";
+		hp = 12;
+		attack = 4; // going to be random +1 | 1-5 damage
+		
+	}
+
+}
